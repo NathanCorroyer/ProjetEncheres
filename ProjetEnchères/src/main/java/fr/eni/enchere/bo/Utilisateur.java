4b5ce5162fb5df445281efa3f9a00780ff31d500
NathanCorroyer <nathan.corroyer8594@gmail.com>
2024-01-29 14:29:48 +0100

    Same

--- ProjetEnchères/src/main/java/fr/eni/enchere/bo/Utilisateur.java
+++ ProjetEnchères/src/main/java/fr/eni/enchere/bo/Utilisateur.java
@@ -13,4 +13,80 @@ public class Utilisateur {
 	private String password;
 	private Integer credit;
 	private boolean administrateur;
+	
+	
+	public Integer getNoUtilisateur() {
+		return noUtilisateur;
+	}
+	public void setNoUtilisateur(Integer noUtilisateur) {
+		this.noUtilisateur = noUtilisateur;
+	}
+	public String getPseudo() {
+		return pseudo;
+	}
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getRue() {
+		return rue;
+	}
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+	public String getCode_postal() {
+		return code_postal;
+	}
+	public void setCode_postal(String code_postal) {
+		this.code_postal = code_postal;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Integer getCredit() {
+		return credit;
+	}
+	public void setCredit(Integer credit) {
+		this.credit = credit;
+	}
+	public boolean isAdministrateur() {
+		return administrateur;
+	}
+	public void setAdministrateur(boolean administrateur) {
+		this.administrateur = administrateur;
+	}
+	
+	
 }
