866feb1fbc90bf3fc11c43695abb5827665737c7
ZIWED <matthieu.lc22@gmail.com>
2024-01-29 13:58:03 +0100

    Connection

--- ProjetEnchères/src/main/java/fr/eni/enchere/dal/ConnectionProvider.java
+++ ProjetEnchères/src/main/java/fr/eni/enchere/dal/ConnectionProvider.java
@@ -1,5 +1,40 @@
 package fr.eni.enchere.dal;
 
-public class ConnectionProvider {
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
+public class ConnectionProvider {
+	
+	private static DataSource dataSource;
+
+	/**
+	 * Au chargement de la classe, la DataSource est recherchée dans l'arbre JNDI
+	 */
+	static {
+		Context contex;
+		try {
+			contex = new InitialContext();
+			ConnectionProvider.dataSource = (DataSource) contex.lookup("java:comp/env/jdbc/pool_cnx");
+		} catch (NamingException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Impossible d'accéder à la base de données");
+		}
+	}
+
+	/**
+	 * Cette méthode retourne une connexion opérationnelle issue du pool de
+	 * connexion vers la base de donnee
+	 * 
+	 * @return
+	 * @throws SQLException
+	 */
+	public static Connection getConnection() throws SQLException {
+		return ConnectionProvider.dataSource.getConnection();
+
+}
+	
 }
