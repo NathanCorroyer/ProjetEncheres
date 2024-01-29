80d133c94ad5aea23406003f7642dd505b7179ce
NathanCorroyer <nathan.corroyer8594@gmail.com>
2024-01-29 15:31:19 +0100

    Merge por favor

---
+++ ProjetEnchères/src/main/java/fr/eni/enchere/controller/ServletRegisterUtilisateur.java
@@ -0,0 +1,34 @@
+package fr.eni.enchere.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletRegisterUtilisateur
+ */
+@WebServlet("/Register")
+public class ServletRegisterUtilisateur extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/register.jps");
+			rd.forward(request, response);
+		}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		UtilisateurManager um = new UtilisateurManager.getInstance();
+		
+	}
+
+}
