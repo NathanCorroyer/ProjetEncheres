1b6276a0fa51c6e2fdaaa7d57290ab80a95952eb
NathanCorroyer <nathan.corroyer8594@gmail.com>
2024-01-30 10:50:46 +0100

    UtilisateurManager

--- ProjetEnchères/src/main/java/fr/eni/enchere/bll/UtilisateurManager.java
+++ ProjetEnchères/src/main/java/fr/eni/enchere/bll/UtilisateurManager.java
@@ -1,13 +1,13 @@
 package fr.eni.enchere.bll;
 
-<<<<<<< HEAD
+
 import fr.eni.enchere.bo.Utilisateur;
-=======
+
+
 import java.sql.SQLException;
 
 import fr.eni.enchere.bo.Utilisateur;
 import fr.eni.enchere.dal.DAOFactory;
->>>>>>> branch 'main' of https://github.com/NathanCorroyer/ProjetEncheres.git
 import fr.eni.enchere.dal.UtilisateurDAO;
 
 public class UtilisateurManager {
@@ -21,22 +21,13 @@ public class UtilisateurManager {
 	
 	public static UtilisateurManager getInstance() {
 		if (instance == null) {
-<<<<<<< HEAD
-			instance = new UtilisateurManager(UtilisateurDAO.getInstance());
-=======
 			instance = new UtilisateurManager(DAOFactory.getUtilisateurDAO());
->>>>>>> branch 'main' of https://github.com/NathanCorroyer/ProjetEncheres.git
+
 		}
 		return instance;
 	}
 	
-<<<<<<< HEAD
-	public Utilisateur login(String email, String password) {
-		return utilisateurDAO.login(email,password);
-		
-		// TO DO METHODE UTILISATEURDAO POUR LOGIN
-	}
-=======
+
 
 	public void deleteByMail(String email) throws SQLException {
 		utilisateurDAO.deleteByMail(email);
@@ -55,10 +46,9 @@ public class UtilisateurManager {
 		try {
 			user = utilisateurDAO.login(email, mdp);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return user;
 	}
 }
->>>>>>> branch 'main' of https://github.com/NathanCorroyer/ProjetEncheres.git
+
