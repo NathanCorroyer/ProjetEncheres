d138b01a299dedfece640b7a98dd1e824b039c16
NathanCorroyer <nathan.corroyer8594@gmail.com>
2024-01-30 15:31:11 +0100

    ArticleDAO

--- ProjetEnchères/src/main/java/fr/eni/enchere/dal/ArticleDAO.java
+++ ProjetEnchères/src/main/java/fr/eni/enchere/dal/ArticleDAO.java
@@ -1,5 +1,20 @@
 package fr.eni.enchere.dal;
 
-public interface ArticleDAO {
+import java.util.List;
 
+import fr.eni.enchere.bo.Article;
+import fr.eni.enchere.bo.Utilisateur;
+
+public interface ArticleDAO {
+	List<Article> selectAll();			//Lister 
+	List<Article> selectByCategorie(); //Filtrer résultats par catégorie
+	List<Article> selectByName(); //Surement un name like ? pour filtre de recherche par nom
+	List<Article> selectArticlesFromUser(Utilisateur u); //Pour afficher tous les articles d'un même utilisateur
+	void ajouter(Article a);
+	void supprimer(Article a);
+	void update(Article a);
+	void deleteSingleArticleFromUser(Utilisateur u, Article a);
+	void deleteAllArticlesFromUser(Utilisateur u);
+	
 }
+
