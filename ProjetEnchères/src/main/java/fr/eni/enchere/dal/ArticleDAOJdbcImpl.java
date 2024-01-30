ee78582fecf674bcbb5e23cc582c457dae341808
NathanCorroyer <nathan.corroyer8594@gmail.com>
2024-01-30 16:10:19 +0100

    Merge branch 'main' of https://github.com/NathanCorroyer/ProjetEncheres.git into main

--- ProjetEnchères/src/main/java/fr/eni/enchere/dal/ArticleDAOJdbcImpl.java
+++ ProjetEnchères/src/main/java/fr/eni/enchere/dal/ArticleDAOJdbcImpl.java
@@ -1,5 +1,64 @@
 package fr.eni.enchere.dal;
 
+import java.util.List;
+
+import fr.eni.enchere.bo.Article;
+import fr.eni.enchere.bo.Utilisateur;
+
 public class ArticleDAOJdbcImpl implements ArticleDAO{
+
+	@Override
+	public List<Article> selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Article> selectByCategorie() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Article> selectByName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Article> selectArticlesFromUser(Utilisateur u) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void ajouter(Article a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void supprimer(Article a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Article a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteSingleArticleFromUser(Utilisateur u, Article a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAllArticlesFromUser(Utilisateur u) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
