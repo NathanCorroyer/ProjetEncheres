bb2a229a1d27ce8f158df9d1ebe0d06e9ced9f95
Matthieu <Matthieu@DESKTOP-VOR0BL9.home>
2024-01-31 10:43:56 +0100

    Methode SelectByPseudo UtilisateurJDBCImpl

--- ProjetEnchères/src/main/java/fr/eni/enchere/bll/UtilisateurManager.java
+++ ProjetEnchères/src/main/java/fr/eni/enchere/bll/UtilisateurManager.java
@@ -53,25 +53,15 @@ public class UtilisateurManager {
 		return utilisateurDAO.findAll();
 	}
 	
-	public Utilisateur selectUserByPseudo(String pseudo) throws BusinessException {
+	public Utilisateur selectUserByPseudo(String pseudo) throws SQLException {
        
 		Utilisateur user = null;
 
         try {
-            // Ici, vous pourriez ajouter des vérifications supplémentaires si nécessaire
-            // par exemple, vérifier si le pseudo est non null ou non vide.
-
-            user= utilisateurDAO.selectByPseudo(pseudo);
-
-            if (user == null) {
-                // Gérer le cas où l'utilisateur n'est pas trouvé
-                throw new BusinessException("Utilisateur avec le pseudo " + pseudo + " non trouvé.");
-            }
-        } catch (Exception e) {
-            // Gérer les exceptions et les erreurs de la base de données
-            throw new BusinessException("Erreur lors de la récupération de l'utilisateur par pseudo", e);
-        }
-
+			user = utilisateurDAO.selectByPseudo(pseudo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
         return user;
     }
 }
