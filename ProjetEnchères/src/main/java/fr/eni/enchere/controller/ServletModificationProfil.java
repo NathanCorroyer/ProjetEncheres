cb37122b30fd2c57445f95c07f9070a288a1438a
Vraën <christophehoarau14@gmail.com>
2024-01-31 11:14:38 +0100

    Merge branch 'main' of https://github.com/NathanCorroyer/ProjetEncheres.git into main

--- ProjetEnchères/src/main/java/fr/eni/enchere/controller/ServletModificationProfil.java
+++ ProjetEnchères/src/main/java/fr/eni/enchere/controller/ServletModificationProfil.java
@@ -1,11 +1,17 @@
 package fr.eni.enchere.controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.enchere.bll.UtilisateurManager;
+import fr.eni.enchere.bo.Utilisateur;
 
 /**
  * Servlet implementation class ServletModificationProfil
@@ -13,11 +19,36 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ServletModificationProfil")
 public class ServletModificationProfil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
+	protected void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		UtilisateurManager connectedUser = (UtilisateurManager) request.getSession().getAttribute("userConnected");
+		request.setAttribute("user", connectedUser);
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/modifier_Profil.jsp");
+		rd.forward(request, response);
+	}
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Utilisateur user = new Utilisateur( request.getParameter("pseudo"),
+											request.getParameter("nom"),
+											request.getParameter("prenom"),
+											request.getParameter("email"),
+											request.getParameter("telephone"),
+											request.getParameter("rue"), 
+											request.getParameter("codePostal"),
+											request.getParameter("ville"),
+											request.getParameter("motDePasse"));
+		user.setNoUtilisateur(Integer.parseInt(request.getParameter("noUtilisateur")));
+		
+		try {
+			UtilisateurManager.getInstance().update(user);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		response.sendRedirect("mon_profil");
+																			
 		
 	}
 
