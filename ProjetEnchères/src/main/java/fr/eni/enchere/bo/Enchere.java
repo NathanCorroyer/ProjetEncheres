d87a000785de6e0d73b6c98561cf4916816ef1d0
Sushiette <itomane57@gmail.com>
2024-02-01 17:05:38 +0100

    merci au roi kris pour les superbes explications

--- ProjetEnchères/src/main/java/fr/eni/enchere/bo/Enchere.java
+++ ProjetEnchères/src/main/java/fr/eni/enchere/bo/Enchere.java
@@ -1,5 +1,52 @@
 package fr.eni.enchere.bo;
 
 public class Enchere {
-
+	private String date_enchere;
+	private String montant_enchere;
+	private Article article;
+	private Utilisateur utilisateur;
+	public String getDate_enchere() {
+		return date_enchere;
+		
+	}
+	public void setDate_enchere(String date_enchere) {
+		this.date_enchere = date_enchere;
+	}
+	public String getMontant_enchere() {
+		return montant_enchere;
+	}
+	public void setMontant_enchere(String montant_enchere) {
+		this.montant_enchere = montant_enchere;
+	}
+	public Article getArticle() {
+		return article;
+	}
+	public void setArticle(Article article) {
+		this.article = article;
+	}
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+	public Enchere(String date_enchere, String montant_enchere, Article article, Utilisateur utilisateur) {
+		super();
+		this.date_enchere = date_enchere;
+		this.montant_enchere = montant_enchere;
+		this.article = article;
+		this.utilisateur = utilisateur;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return String.format("enchère || date enchère : %s, montant enchère %s, article %s, utilisateur %s", this.date_enchere, this.montant_enchere, this.article.getNom_Article(), this.utilisateur.getPseudo() );
+	}
+	
+	public Enchere() {
+		
+	}
+	
 }
