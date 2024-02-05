94b24d0b9a1bba96693b345386fe66cb82414f05
Sushiette <itomane57@gmail.com>
2024-02-05 10:21:47 +0100

    commit messahe

--- ProjetEnchères/src/main/java/fr/eni/enchere/controller/ServletRecuperationListeCategorie.java
+++ ProjetEnchères/src/main/java/fr/eni/enchere/controller/ServletRecuperationListeCategorie.java
@@ -1,11 +1,17 @@
 package fr.eni.enchere.controller;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.enchere.bll.CategorieManager;
+import fr.eni.enchere.bo.Categorie;
 
 /**
  * Servlet implementation class ServletRecuperationListeCategorie
@@ -25,7 +31,12 @@ public class ServletRecuperationListeCategorie extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		CategorieManager cm = CategorieManager.getInstance();
+		List<Categorie> listeCategorie = cm.selectAll(); 
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+        request.setAttribute("listeCategorie" , listeCategorie);
+        dispatcher.forward(request, response);
+
 	}
 
 	/**
