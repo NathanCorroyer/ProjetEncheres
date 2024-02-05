3beda27c9591e6bf256c5a4c247f36d8248c8fc8
Vraën <christophehoarau14@gmail.com>
2024-02-05 11:35:09 +0100

    Merge branch 'main' of https://github.com/NathanCorroyer/ProjetEncheres.git into main

--- ProjetEnchères/src/main/java/fr/eni/enchere/controller/ServletAffichantProfilVendeur.java
+++ ProjetEnchères/src/main/java/fr/eni/enchere/controller/ServletAffichantProfilVendeur.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import fr.eni.enchere.bll.BLLException;
 import fr.eni.enchere.bll.UtilisateurManager;
@@ -25,22 +26,42 @@ public class ServletAffichantProfilVendeur extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
 		String pseudoUser = (String) request.getParameter("userPseudo");
 		UtilisateurManager um = UtilisateurManager.getInstance();
+		Utilisateur userConnected = (Utilisateur) session.getAttribute("userConnected");
 		Utilisateur userAffiche = null ;
 		
 		
+		
+		if ( userConnected != null ) {
+			String pseudoUserConnected = userConnected.getPseudo();
+			if ( pseudoUserConnected.equals(pseudoUser) ) {
+				request.setAttribute("userConnected", userConnected );
+				request.getRequestDispatcher("/WEB-INF/jsp/mon_profil.jsp").forward(request, response);	
+			}	else {
+					try {
+						userAffiche = um.selectUserByPseudo(pseudoUser);
+					} catch (SQLException | BLLException e) {
+						e.printStackTrace();
+				}
+				request.setAttribute("userAffiche", userAffiche);
+				request.getRequestDispatcher("/WEB-INF/jsp/AffichageUtilisateurVendeur.jsp").forward(request, response);
+			}
+			
+		} else {
+			
 			try {
 				userAffiche = um.selectUserByPseudo(pseudoUser);
 			} catch (SQLException | BLLException e) {
 				e.printStackTrace();
 			}
+			request.setAttribute("userAffiche", userAffiche);
+			request.getRequestDispatcher("/WEB-INF/jsp/AffichageUtilisateurVendeur.jsp").forward(request, response);
 		
-		request.setAttribute("userAffiche", userAffiche);
-		request.getRequestDispatcher("/WEB-INF/jsp/AffichageUtilisateurVendeur.jsp").forward(request, response);
-		
-		
-	}
+		}
+	
+}
 }
 
 
